53ce481ba093f3ab487777ca17f548e77f8419fd
Aroh Gahane <102295478+arohgahane@users.noreply.github.com>
2022-04-24 20:16:42 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,19 @@
+/console.log("print the written code or text")/ 
+console.log("Working....."); 
+console.log("Hello World!!!"); /Check the output we are getting for a particular operation in console/
+
+var number = 2;
+function Square(n) {
+    var result = n*n;
+    return result;
+}
+
+var newNumber = Square(3); //operation 1 using call stack
+
+let a = 3;
+let b = 6;
+
+let add = a + b; //operation 2 using call stack
+let sub = a - b; //operation 3 using call stack
+console.log(add);
+console.log(sub);
